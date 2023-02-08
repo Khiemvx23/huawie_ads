2f324bb2f94f2e54faf65196b87d4ab645953b3b
ngocji <dngocx@gmail.com>
2023-02-08 11:13:20 +0700

    Update adutils

--- app/src/main/java/com/WAStickersApp/animatedstickers/morning/utils/AdUtils.java
+++ app/src/main/java/com/WAStickersApp/animatedstickers/morning/utils/AdUtils.java
@@ -7,10 +7,13 @@ import com.huawei.hms.ads.AdListener;
 import com.huawei.hms.ads.AdParam;
 import com.huawei.hms.ads.InterstitialAd;
 import com.huawei.hms.ads.banner.BannerView;
+import com.huawei.hms.ads.nativead.NativeAd;
+import com.huawei.hms.ads.nativead.NativeAdLoader;
 
 public class AdUtils {
     private static final String BANNER_ID = "testw6vs28auh3";
     private static final String INTERSTITIAL_ID = "testb4znbuh3n2";
+    private static final String NATIVE_BANNER_ID = "testy63txaom86";
 
     public static void loadBannerAd(BannerView bannerView) {
         bannerView.setAdId(BANNER_ID);
@@ -24,21 +27,28 @@ public class AdUtils {
         }
     }
 
-    public static InterstitialAd loadInterstitialAd(Context context) {
+    public static InterstitialAd loadInterstitialAd(Context context, AdListener adListener) {
         InterstitialAd interstitialAd = new InterstitialAd(context);
         interstitialAd.setAdId(INTERSTITIAL_ID);
 
         AdParam adParam = new AdParam.Builder().build();
         interstitialAd.loadAd(adParam);
+        interstitialAd.setAdListener(adListener);
         return interstitialAd;
     }
 
-    public static void showInterstitialAd(Activity activity, InterstitialAd ad, AdListener adListener, Runnable onFailedRunnable) {
+    public static void showInterstitialAd(Activity activity, InterstitialAd ad, Runnable onFailedRunnable) {
         if (ad != null && ad.isLoaded()) {
-            ad.setAdListener(adListener);
             ad.show(activity);
         } else {
             if (onFailedRunnable != null) onFailedRunnable.run();
         }
+    }
+
+    public static void loadNativeAd(Context context, NativeAd.NativeAdLoadedListener nativeLoadedListener, AdListener adListener) {
+        NativeAdLoader.Builder builder = new NativeAdLoader.Builder(context, NATIVE_BANNER_ID);
+        builder.setNativeAdLoadedListener(nativeLoadedListener).setAdListener(adListener);
+        NativeAdLoader nativeAdLoader = builder.build();
+        nativeAdLoader.loadAd(new AdParam.Builder().build());
     }
 }
